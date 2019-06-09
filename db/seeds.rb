786ac695b2c1930863148b94a5f72226ea2e20d4
Vanessa Frenkel <vanefrenkel@gmail.com>
2019-06-09 19:20:31 -0400

    added seed info users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,39 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-    addresses = "289 Constitution Court
-    Liverpool, NY 13090
-    8248 Buttonwood Drive
-    Herndon, VA 20170
-    953 Lake View Lane
-    Greensburg, PA 15601
-    8489 Smith Store Street
-    Wake Forest, NC 27587
-    60 Pumpkin Hill Dr.
-    South Ozone Park, NY 11420
-    923 Wakehurst Lane
-    New Orleans, LA 70115
-    995 Lakewood Court
-    Upland, CA 91784
-    "
-    Shop.destroy_all
-    addresses = addresses.split("\n")
-    addresses.each do |address|
-    Shop.create!(name: Faker::Name.name, address: address)
-    sleep 1
-    end
+
+
+
+
+
+    # addresses = "289 Constitution Court
+    # Liverpool, NY 13090
+    # 8248 Buttonwood Drive
+    # Herndon, VA 20170
+    # 953 Lake View Lane
+    # Greensburg, PA 15601
+    # 8489 Smith Store Street
+    # Wake Forest, NC 27587
+    # 60 Pumpkin Hill Dr.
+    # South Ozone Park, NY 11420
+    # 923 Wakehurst Lane
+    # New Orleans, LA 70115
+    # 995 Lakewood Court
+    # Upland, CA 91784
+    # "
+    # Shop.destroy_all
+    # addresses = addresses.split("\n")
+    # addresses.each do |address|
+    # Shop.create!(name: Faker::Name.name, address: address)
+    # sleep 1
+    # end
+    
+    
+    # User.destroy_all
+    # User.create(name:'vane', lastname:'Frenkel', email:'vanefrenkel@gmail.com',password:'Pass1010',role:'admin')
+    # 10.times do |user| 
+    #     User.create(name: Faker::Name.name,lastname:Faker::Lastname.lastname, password: 'Pass1010', email: "user-#{user}", role: 'client')
+    # end
+
+
+
